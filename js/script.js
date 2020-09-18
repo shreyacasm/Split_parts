a7bbff63dbef6ab2902ea3148c200f5bd6119950
Shreya Suroliya <suroliya.shreya2014@gmail.com>
2020-09-18 21:47:34 +0530

    js added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var counter = 1;
+var limit = 6;
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = "Person " + (counter + 2) + " <br><input type='text' name='myInputs[]'>";
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
+}
+
